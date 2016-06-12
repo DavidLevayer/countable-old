d9475b58e153a755c32c13155d7a1ac2b429d84f
David Levayer <david.levayer@gmail.com>
2016-06-12 21:24:26 +0200

    sidebar for app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'app/app.component.html',
+    styleUrls: ['app/app.component.css']
 })
 
 export class AppComponent { }
