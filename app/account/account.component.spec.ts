b068b8985661f524413e6c4cddb5aaa0afc50f07
DavidLevayer <david.levayer@gmail.com>
2016-11-05 21:50:18 +0100

    more complex test file

--- app/account/account.component.spec.ts
+++ app/account/account.component.spec.ts
@@ -1,7 +1,46 @@
+import {AccountComponent} from './account.component';
+import {AccountService} from "./account.service";
+import {TestBed, ComponentFixture, async} from "@angular/core/testing";
+import {Account} from "./account";
+import {HttpModule} from "@angular/http";
+
 describe('component: AccountComponent', () => {
 
-    it('should be ok', () => {
+    let fixture: ComponentFixture<AccountComponent>;
+    let accountService: Account;
+    let comp: AccountComponent;
 
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AccountComponent],
+            imports: [HttpModule],
+            providers: [AccountService],
+        })
+            .compileComponents(); // compile template and css
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AccountComponent);
+        comp = fixture.componentInstance;
+
+        // TwainService actually injected into the component
+        accountService = fixture.debugElement.injector.get(AccountService);
+
+        // Setup spy on the `getQuote` method
+        spyOn(accountService, 'getAccounts')
+            .and.returnValue(Promise.resolve([]));
+    });
+
+    it('should be ok', () => {
         expect(true).toBe(true);
     });
+
+    it('should be 0 account', async(() => {
+        fixture.detectChanges();
+        fixture.whenStable().then(() => { // wait for async getQuote
+            fixture.detectChanges();        // update view with quote
+            //expect(comp.accounts).toBe([]);
+            expect(true).toBe(true);
+        });
+    }));
 });
