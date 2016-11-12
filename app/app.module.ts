14d68d42b5c8a954fe0656f640a382cab1ac4e65
DavidLevayer <david.levayer@gmail.com>
2016-11-12 21:40:46 +0100

    Add Angular material

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,11 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AccountComponent } from './account/account.component';
+import {FormsModule} from '@angular/forms';
+import {MaterialModule} from '@angular/material';
 
 @NgModule({
     bootstrap:    [ AppComponent ],
     declarations: [ AppComponent, DashboardComponent, AccountComponent ],
-    imports:      [ BrowserModule, AppRoutingModule, HttpModule ],
+    imports:      [ BrowserModule, AppRoutingModule, HttpModule, MaterialModule.forRoot(), FormsModule ],
     providers: [],
 })
 export class AppModule { }
