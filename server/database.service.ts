ffe7002b881e691a026331fd8c8797bc4bd22b22
DavidLevayer <david.levayer@gmail.com>
2016-11-20 11:38:53 +0100

    Add Category and Subcategory tables to database

--- server/database.service.ts
+++ server/database.service.ts
@@ -3,6 +3,20 @@ const accountTableQuery: string =
     'accountId INTEGER PRIMARY KEY,' +
     'name TEXT NOT NULL);';
 
+const categoryTableQuery: string =
+    'CREATE TABLE IF NOT EXISTS Category(' +
+    'categoryId INTEGER PRIMARY KEY,' +
+    'name TEXT NOT NULL);';
+
+const subcategoryTableQuery: string =
+    'CREATE TABLE IF NOT EXISTS Subcategory(' +
+    'categoryId INTEGER PRIMARY KEY,' +
+    'name TEXT NOT NULL,' +
+    'refCategory INTEGER NOT NULL,' +
+    'FOREIGN KEY(refCategory) REFERENCES Category(categoryId));';
+
 export const populationQueries: string[] = [
     accountTableQuery,
+    categoryTableQuery,
+    subcategoryTableQuery
 ];
